22e0f0aa0b0f9d925c926ba21fa5882c9db6fc7d
yeeway <alex20020609@gmail.com>
2024-01-04 16:05:40 +0800

    update

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import App from "./App";
 const rootElement = document.getElementById("root");
 ReactDOM.render(
   <StrictMode>
-    <BrowserRouter basename="/uniqlo-store-map"> {/* 為了部署到GitHub Pages上，要加上basename */}
+    <BrowserRouter basename="/uniqlo-store-map-clone"> {/* 為了部署到GitHub Pages上，要加上basename */}
       <App />
     </BrowserRouter>
   </StrictMode>,
